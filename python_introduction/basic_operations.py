240b68dda423cedac75212daa4d37b070e84533c
Denis Ndiritu <denisndiritu1@gmail.com>
2025-07-08 19:09:25 +0300

    Performed addition, subtraction, and division operations on two numbers and printed their outputs

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+# variable declaration
+number1 = 23
+number2 = 90
+
+# Arithmetic operations
+print(f"Addition of {number1} and {number2} is {number1 + number2}")
+print(f"Subtraction of {number2} and {number1} is {number2 - number1}")
+print(f"Multiplication of {number1} and {number2} is {number1 * number2}")
