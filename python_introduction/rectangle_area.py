3d24b2a9997d15ec720fc4387687090a457b8621
Denis Ndiritu <denisndiritu1@gmail.com>
2025-07-08 19:19:18 +0300

    Created a simple area calculator

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,8 @@
+# User basic python arithmetic operations and variable
+# assignmens to calculate the area of a rectangle.
+
+length = 10
+width = 5
+
+area = length * width
+print(f"The area of the rectangle is: {area}")
