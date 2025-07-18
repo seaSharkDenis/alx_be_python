50508ab604521fe1fb5026846e03037006593dc5
Denis Ndiritu <denisndiritu1@gmail.com>
2025-07-18 11:57:06 +0300

    Created a daily reminder applicaion that acceps a task, its priority, and time bound status as inputs and provides appropriate outputs foe each criteria

---
+++ control-flow/daily_reminder.py
@@ -0,0 +1,22 @@
+task = input("Enter you task description: ")
+priority= input("Enter your task's prioriyt (high, medium, low): ").lower()
+time_bound = input("Is the task time-bound? (yes or no) ").lower()
+
+match priority:
+    case "high":
+        if time_bound == "yes":
+            print(f"Reminder: '{task}' is a high priority tasks that required immediate attention today!")
+        else:
+            print(f"Note: '{task}' is a high priority task. Consider completing it when you have free time.")
+    case "medium":
+        if time_bound == "yes":
+            print(f"Reminder: '{task}' is a medium priority task.")
+        else:
+            print(f"Note: '{task}' is not timebound. Consider completing it once you have time.")
+    case "low":
+        if time_bound == "yes":
+            print(f"Reminder: '{task}' is a low priority task. It however required you to allocate some time for it")
+        else:
+            print(f"Note: '{task}' is a low priority. Consider compelting it when you have free time.")
+    case _:
+        print("Invalid input, please enter a value withing the range provided.")
