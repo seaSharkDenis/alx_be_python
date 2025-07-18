7b1ffb49b485a012bce11548facb42e8bb39cf3a
Denis Ndiritu <denisndiritu1@gmail.com>
2025-07-18 08:38:41 +0300

    Renamed the weather_today variable to weather

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,12 +1,12 @@
 # Weather Recommendation Program
 
-weather_today = input("What's the weather like today? (sunny/rainy/cold): ").lower()
+weather = input("What's the weather like today? (sunny/rainy/cold): ").lower()
 
-if weather_today == "sunny":
+if weather == "sunny":
     print("Wear a t-shirt and sunglasses.")
-elif weather_today == "rainy":
+elif weather == "rainy":
     print("Don't forget your umbrella and a raincoat.")
-elif weather_today == "cold":
+elif weather == "cold":
     print("Make sure to wear a warm coat and a scarf.")
 else:
     print("Sorry, I don't have recommendations for this weather.")
