6178c2dd9046fa5ab77352acab75e94a7a2d117b
Denis Ndiritu <denisndiritu1@gmail.com>
2025-07-23 09:57:26 +0300

    Replaced the match case with an if..elif statement

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,15 +1,14 @@
 def perform_operation(num1, num2, operation):
     result = 0
-    match operation:
-        case "add":
-            result = num1 + num2
-        case "subtract":
-            result = num1 - num2
-        case "multiply":
-            result = num1 * num2
-        case "divide":
-            if num2 == 0:
-                result = "Cannot divide by zero."
-            else:
-                result = num1 / num2
+    if operation == "add":
+        result = num1 + num2
+    elif operation == "subtract":
+        result = num1 - num2
+    elif operation == "multiply":
+        result = num1 * num2
+    elif operation == "divide":
+        if num2 == 0:
+            result = "Cannot divide by zero."
+        else:
+            result = num1 / num2
     return result
