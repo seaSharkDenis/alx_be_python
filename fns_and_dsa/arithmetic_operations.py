754574e30e6524896988c17595d8ea826791f17b
Denis Ndiritu <denisndiritu1@gmail.com>
2025-07-23 09:47:49 +0300

    Created an arithmetic operations function and imported it into the main.py file

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,15 @@
+def perform_operation(num1, num2, operation):
+    result = 0
+    match operation:
+        case "add":
+            result = num1 + num2
+        case "minus":
+            result = num1 - num2
+        case "multiply":
+            result = num1 * num2
+        case "divide":
+            if num2 == 0:
+                result = "Cannot divide by zero."
+            else:
+                result = num1 / num2
+    return result
