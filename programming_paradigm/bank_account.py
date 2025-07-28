a1d50ed8d1713b9c6dd05e2f903d340d6c78a7c4
Denis Ndiritu <denisndiritu1@gmail.com>
2025-07-28 21:32:34 +0300

    Updated the display balance statement to include a dollar sign

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -12,5 +12,5 @@ class BankAccount:
         return False
 
     def display_balance(self):
-        balance = f"Current Balance: {self.account_balance}"
+        balance = f"Current Balance: ${self.account_balance}"
         print(balance)
