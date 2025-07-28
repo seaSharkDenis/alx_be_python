00696085a5b41e394c532b249ccd10c46bee3bb5
Denis Ndiritu <denisndiritu1@gmail.com>
2025-07-28 21:51:58 +0300

    Implemented a safe divide function allowing a user to divide two numbers. The function handles division by zero and non-numeric inputs exceptions

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,11 @@
+def safe_divide(numerator, denominator):
+    try:
+        numerator_float = float(numerator)
+        denominator_float = float(denominator)
+
+        result = numerator_float / denominator_float
+        return f"The result of your division is {result}"
+    except ZeroDivisionError:
+        print("Error: Cannot divide by zero")
+    except ValueError:
+        print("Error: Please enter numeric values only.")
