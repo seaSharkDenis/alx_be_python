4af07c958819767c81b53a985bee148e4c66b39e
Denis Ndiritu <denisndiritu1@gmail.com>
2025-08-07 14:17:47 +0300

    Created a Base Class Shape with an area() method. Created two classes, Rectangle, and Circle, which are derived from the base class Shape. The two derived classes overrides the area() method to calculate the respective areas

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,23 @@
+import math
+
+
+class Shape:
+    def area(self):
+        return NotImplementedError
+
+
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+
+
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * (self.radius * self.radius)
